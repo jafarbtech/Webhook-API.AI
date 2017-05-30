9935f3e35bcd77a491920557f312b661f8896f75
jafarbtech <jafarbtech@users.noreply.github.com>
2017-05-30 15:52:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
+    if(req.body.action.type === 'echo') {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     return res.json({
         speech: speech,
         displayText: speech,
         source: 'webhook-echo-sample'
     });
+    }
 });
 
 restService.listen((process.env.PORT || 8000), function() {
