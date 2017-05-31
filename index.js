7f0cdbf7c08bcef8d92da90f8c1ce67a4ba314e0
jafarbtech <jafarbtech@users.noreply.github.com>
2017-05-31 11:15:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,50 +21,47 @@ restService.post('/echo', function(req, res) {
             displayText: speech,
             source: 'webhook-echo-sample'
         });
-    } else if (req.body.result.action === 'weather') {        
+    } else if (req.body.result.action === 'weather') {
+        var speech = 'Seems like some problem. Speak1 again.';
         if (req.body.result && req.body.result.parameters && req.body.result.parameters.city) {
-            invokeAndGetResponse(req.body.result.parameters.city,function(err, result){
-                if(err){
-                    var speech = 'Seems like some problem. Speak again.';
-                    console.log("Got an error: ", err);
+            var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text=%27' + escape(req.body.result.parameters.city) + '%27)&format=json';
+            //console.log("url: ", url);
+            http.get(url, function(resp) {
+                var body = '';
+
+                resp.on('data', function(chunk) {
+                    body += chunk;
+                });
+
+                resp.on('end', function() {
+                    var fbResponse = JSON.parse(body);
+                    speech = 'Today in ' + fbResponse.query.results.channel.location.city + ': ' + fbResponse.query.results.channel.item.condition.text + ', the temperature is ' + fbResponse.query.results.channel.item.condition.temp + ' ' + fbResponse.query.results.channel.units.temperature;
+                    //console.log("Got a response: ", body);
+                    console.log("speech: ", speech);
                     return res.json({
                         speech: speech,
                         displayText: speech,
                         source: 'webhook-echo-sample'
                     });
-                } else {
-                    var speech = result;
-                    console.log("result: ", result);
-                    return res.json({
-                        speech: speech,
-                        displayText: speech,
-                        source: 'webhook-echo-sample'
-                    });
-                }
+                });
+            }).on('error', function(e) {
+                console.log("Got an error: ", e);
+                return res.json({
+                    speech: speech,
+                    displayText: speech,
+                    source: 'webhook-echo-sample'
+                });
+            });
+            console.log("res.json: ", speech);
+            return res.json({
+                speech: speech,
+                displayText: speech,
+                source: 'webhook-echo-sample'
             });
         }
     }
 });
-var invokeAndGetResponse = function(city,callback){
-    var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text=%27' + escape(city) + '%27)&format=json';
-            console.log("url: ", url);
-            http.get(url, function(res) {
-                var body = '';
 
-                res.on('data', function(chunk) {
-                    body += chunk;
-                });
-
-                res.on('end', function() {
-                    var fbResponse = JSON.parse(body);
-                    console.log("Got a response: ", body);
-                    var speech = 'Today in ' + fbResponse.query.results.channel.location.city + ': ' + fbResponse.query.results.channel.item.condition.text + ', the temperature is ' + fbResponse.query.results.channel.item.condition.temp + ' ' + fbResponse.query.results.channel.units.temperature;
-                    callback(null,speech);
-                });
-            }).on('error', function(e) {
-                callback(e);
-            });
-}
 restService.listen((process.env.PORT || 8000), function() {
     console.log("Server up and listening");
 });
