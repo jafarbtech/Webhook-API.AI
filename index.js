f45d83763d56f930a157b5c5fe3ddb7a7b264c4d
jafarbtech <jafarbtech@users.noreply.github.com>
2017-05-31 11:02:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,10 +21,32 @@ restService.post('/echo', function(req, res) {
             displayText: speech,
             source: 'webhook-echo-sample'
         });
-    } else if (req.body.result.action === 'weather') {
-        var speech = 'Seems like some problem. Speak1 again.';
+    } else if (req.body.result.action === 'weather') {        
         if (req.body.result && req.body.result.parameters && req.body.result.parameters.city) {
-            var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text=%27' + escape(req.body.result.parameters.city) + '%27)&format=json';
+            invokeAndGetResponse(function(err, result){
+                if(err){
+                    var speech = 'Seems like some problem. Speak again.';
+                    console.log("Got an error: ", err);
+                    return res.json({
+                        speech: speech,
+                        displayText: speech,
+                        source: 'webhook-echo-sample'
+                    });
+                } else {
+                    var speech = result;
+                    console.log("result: ", result);
+                    return res.json({
+                        speech: speech,
+                        displayText: speech,
+                        source: 'webhook-echo-sample'
+                    });
+                }
+            });
+        }
+    }
+});
+var invokeAndGetResponse = function(city,callback){
+    var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text=%27' + escape(city) + '%27)&format=json';
             //console.log("url: ", url);
             http.get(url, function(res) {
                 var body = '';
@@ -37,31 +59,12 @@ restService.post('/echo', function(req, res) {
                     var fbResponse = JSON.parse(body);
                     speech = 'Today in ' + fbResponse.query.results.channel.location.city + ': ' + fbResponse.query.results.channel.item.condition.text + ', the temperature is ' + fbResponse.query.results.channel.item.condition.temp + ' ' + fbResponse.query.results.channel.units.temperature;
                     //console.log("Got a response: ", body);
-                    console.log("speech: ", speech);
-                    return res.json({
-                        speech: speech,
-                        displayText: speech,
-                        source: 'webhook-echo-sample'
-                    });
+                    callback(null,speech);
                 });
             }).on('error', function(e,res) {
-                console.log("Got an error: ", e);
-                return res.json({
-                    speech: speech,
-                    displayText: speech,
-                    source: 'webhook-echo-sample'
-                });
+                callback(e);
             });
-            console.log("res.json: ", speech);
-            return res.json({
-                speech: speech,
-                displayText: speech,
-                source: 'webhook-echo-sample'
-            });
-        }
-    }
-});
-
+}
 restService.listen((process.env.PORT || 8000), function() {
     console.log("Server up and listening");
 });
