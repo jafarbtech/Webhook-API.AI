1e865bd18250f6cdbde277a65c6d4a46d9bf68c8
jafarbtech <jafarbtech@users.noreply.github.com>
2017-05-31 13:13:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,6 @@ restService.post('/echo', function(req, res) {
         }
     }
 });
-
 restService.listen((process.env.PORT || 8000), function() {
     console.log("Server up and listening");
 });
